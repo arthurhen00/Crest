15e9e00c22f0557751aec9c5101be3dbe825b7c5
arthur <arthurhendges@hotmail.com>
2026-06-28 21:39:53 -0300

    feat: improve HttpResponse builder and response headers

--- include/http/HttpResponse.hpp
+++ include/http/HttpResponse.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 namespace http {
 
@@ -12,12 +13,15 @@ public:
   void status(int code);
   void body(const std::string &content);
   void json(const std::string &content);
+  void text(const std::string &content);
+  void header(const std::string &key, const std::string &value);
+
   std::string build() const;
 
 private:
   int status_;
+  std::unordered_map<std::string, std::string> headers_;
   std::string body_;
-  std::string contentType_;
 
   std::string statusMessage() const;
 };
--- src/http/HttpResponse.cpp
+++ src/http/HttpResponse.cpp
@@ -3,7 +3,9 @@
 namespace http {
 
 HttpResponse::HttpResponse()
-: status_(200), contentType_("text/plain") {}
+: status_(200) {
+  headers_["Content-Type"] = "text/plain";
+}
 
 void HttpResponse::status(int code) {
   status_ = code;
@@ -14,27 +16,31 @@ void HttpResponse::body(const std::string &content) {
 }
 
 void HttpResponse::json(const std::string &content) {
-  contentType_ = "application/json";
+  header("Content-type", "application/json");
   body_ = content;
+}
+
+void HttpResponse::text(const std::string &content) {
+  header("Content-type", "text/plain");
+  body_ = content;
+}
+
+void HttpResponse::header(const std::string &key, const std::string &value) {
+  headers_[key] = value;
 }
 
 std::string HttpResponse::statusMessage() const {
   switch (status_) {
     case 200:
       return "OK";
-
     case 201:
       return "Created";
-
     case 400:
       return "Bad Request";
-
     case 404:
       return "Not Found";
-
     case 500:
       return "Internal Server Error";
-
     default:
       return "";
   }
@@ -44,28 +50,23 @@ std::string HttpResponse::build() const {
   std::string response;
 
   response +=
-    "HTTP/1.1 "
-    +
-    std::to_string(status_)
-    +
-    " "
-    +
-    statusMessage()
-    +
+    "HTTP/1.1 " +
+    std::to_string(status_) +
+    " " +
+    statusMessage() +
     "\r\n";
 
-  response +=
-    "Content-Type: "
-    +
-    contentType_
-    +
-    "\r\n";
+  for (const auto& [key, value] : headers_) {
+    response +=
+      key +
+      ": " +
+      value +
+      "\r\n";
+  }
 
   response +=
-    "Content-Length: "
-    +
-    std::to_string(body_.size())
-    +
+    "Content-Length: " +
+    std::to_string(body_.size()) +
     "\r\n";
 
   response +=
